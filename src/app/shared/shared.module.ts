4681c4be0e27a2748a2357a7429c02eba34cd505
Guru Mohan Reddy <gurumohanreddy@users.noreply.github.com>
2016-11-15 15:14:36 +0530

    Deleted components which are not used. (#65)
    * style and disqus fix
    
    * navbar brand to center
    
    * markdown in students view
    
    * codeastra name in navbar
    
    * codeastra title
    
    * deleted unwanted components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,13 +8,8 @@ import { TaskQuestionEditorComponent } from './questions/task-question-editor.co
 import { TaskQuestionComponent } from './questions/task-question.component';
 import { TaskQuestionsComponent } from './questions/task-questions.component';
 import { NavbarComponent } from './navbar/index';
-import { PanelComponent } from './panel/panel.component';
 import { TogglableFormComponent } from './togglable-form/togglable-form.component';
 import { EditableFormComponent, EditableFormContentComponent, EditableFormReadOnlyComponent  } from './editable-form/editable-form.component';
-import { PanelBodyComponent } from './panel-body/panel-body.component';
-import { CardComponent } from './card/card.component';
-import { ListComponent } from './list/list.component';
-import { TileComponent } from './tile/tile.component';
 import { MarkDownViewerComponent } from './markdown/markdown-viewer.component';
 import { FaComponent } from 'angular2-fontawesome/components';
 import { DisqusComponent } from './disqus/disqus.component';
@@ -32,14 +27,13 @@ import { DatePickerComponent } from './date-picker/date-picker.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule, FormsModule, MaterialModule.forRoot(), TagInputModule],
-  declarations: [NavbarComponent, PanelComponent, TogglableFormComponent,
-  ListComponent, PanelBodyComponent, CardComponent, TileComponent,MarkDownViewerComponent,
+  declarations: [NavbarComponent, TogglableFormComponent,
+  MarkDownViewerComponent,
   FaComponent, DisqusComponent, TaskQuestionsComponent, TaskQuestionComponent,
    TaskQuestionEditorComponent, EditableFormComponent,
    EditableFormContentComponent, EditableFormReadOnlyComponent,
    TagsComponent, DatePickerComponent, TaskStatusHighlighterDirective],
-  exports: [NavbarComponent, PanelComponent, TogglableFormComponent, CardComponent,
-  ListComponent, PanelBodyComponent, TileComponent,  CommonModule, FormsModule,
+  exports: [NavbarComponent, TogglableFormComponent, CommonModule, FormsModule,
    RouterModule, FaComponent, MarkDownViewerComponent,
    DisqusComponent,TaskQuestionsComponent,TaskQuestionComponent, TaskQuestionEditorComponent
    , MaterialModule, EditableFormComponent, EditableFormContentComponent,
